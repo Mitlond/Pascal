b332c7b6c5b1f8b4d529b6218be4691c71e6c08a
Mitlond <comp122012016@mail.ru>
2016-04-13 15:18:04 +0300

    Pascal to PHP

---
+++ Kodmod.php
@@ -0,0 +1,34 @@
+function mat_from_pascal($a,$n:m,$i,$j,$d,$k,$p,$sumtmp){
+$tp=$a[$x] [$r+1]
+$a[$x] [$r]=tp;
+for ($i=1;$i<=5;$i++)
+{
+for ($j=1;$m;$j++)
+{
+$a[$i] [$j]=random(10);
+$(a[$i] [$j]);
+}
+}
+$d=1;
+$s[$d]=0;
+for ($p;$m;$p++)
+{
+$sumtmp=$a[$k] [$p];
+$s[$d]=$s[$d]+$sumtmp;
+}
+echo ($s[$d]);
+if ($d!=$m)
+{
+$d=$d+1;
+}
+echo ",";
+if $s[$d+1]=$s[$d+1];
+{
+$temp=$s[$d+1];
+$s[$d+1]=$s[$d];
+$s[$d]=$temp;
+for $sp=($m;$m++);
+{
+echo
+}
+}
